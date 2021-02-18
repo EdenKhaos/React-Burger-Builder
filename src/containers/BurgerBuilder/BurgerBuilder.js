658c054798458ce1f858450b2d35f62ae013b5aa
Heather <raven.dantes@ymail.com>
2021-02-18 15:01:17 -0700

    add Order Now button

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -23,7 +23,21 @@ class BurgerBuilder extends Component {
             cheese: 0,
             meat: 0
         },
-        totalPrice: 4
+        totalPrice: 4,
+        purchasable: false
+    }
+    updatePurchaseState () {
+        const ingredients = {
+            ...this.state.ingredients
+        };
+        const sum = Object.keys(ingredients)
+            .map(igKey => {
+                return ingredients[igKey]
+            })
+            .reduce((sum,el) => {
+                return sum + el;
+            }, 0);
+        this.setState({purchasable: sum > 0});
     }
 
     addIngredientHandler = (type) => {
@@ -70,6 +84,7 @@ class BurgerBuilder extends Component {
                 ingredientAdded={this.addIngredientHandler} 
                 ingredientRemoved={this.removeIngredientHandler} 
                 disabled={disabledInfo} 
+                purchasable={this.state.purchasable}
                 price={this.state.totalPrice} />
             </Aux>
         );
